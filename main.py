899e31c556668f1e148b0ff2987b95bb61e6d16e
Paarthbathla338 <Paarth.bathla@outlook.com>
2021-06-27 13:32:36 +0530

    Plotted Train and Loss Accuracy and Cleanup Completed

--- main.py
+++ main.py
@@ -280,3 +280,21 @@ print(classification_report(testY.argmax(axis=1), predIdxs,
 # serialize the model to disk
 print("[INFO] saving mask detector model...")
 model.save("mask_detector.model", save_format="h5")
+
+# plot the training loss and accuracy
+N = EPOCHS
+plt.style.use("ggplot")
+plt.figure()
+plt.plot(np.arange(0, N), H.history["loss"], label="train_loss")
+plt.plot(np.arange(0, N), H.history["val_loss"], label="val_loss")
+plt.plot(np.arange(0, N), H.history["accuracy"], label="train_acc")
+plt.plot(np.arange(0, N), H.history["val_accuracy"], label="val_acc")
+plt.title("Training Loss and Accuracy")
+plt.xlabel("Epoch #")
+plt.ylabel("Loss/Accuracy")
+plt.legend(loc="lower left")
+plt.savefig("plot.png")
+
+# do a bit of cleanup
+cv2.destroyAllWindows()
+vs.stop()
